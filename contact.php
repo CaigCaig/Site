bd801b48fb3e59237835b58dfd38ab797402de58
CaigCaig <caig@inbox.ru>
2023-05-30 02:09:27 +0300

    2023-05-30 generation

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>Сайт визитка - шаблон</title>
+  <title>Сайт визитка</title>
   <meta name="description" content="Описание" />
   <meta name="keywords" content="ключевые слова" />
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
@@ -37,8 +37,8 @@
         <div class="sidebar">
           <h3>Latest News</h3>
           <h4>New Website Launched</h4>
-          <h5>January 1st, 2012</h5>
-          <p>2012 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
+          <h5>30 мая, 2023</h5>
+          <p>2023 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
         </div>
         <div class="sidebar">
           <h3>Useful Links</h3>
@@ -101,7 +101,7 @@
     </div>
     <footer>
       <p><a href="index.html">Главная</a> | <a href="examples.html">Обо мне</a> | <a href="page.html">Фотографии</a> | <a href="another_page.html">Блок</a> | <a href="contact.php">Контакты</a></p>
-      <p>&copy; 2013. All Rights Reserved. | <a href="http://21ru.ru">21ru.ru</a></p>
+      <p>&copy; 2023. All Rights Reserved. | <a href="http://netology.ru">netology.ru</a></p>
     </footer>
   </div>
   <!-- javascript at the bottom for fast page loading -->
